ffcd4f0243127a482c5c0a51cc3b773a5b8cdc9e
zeroxys <miguelzavalac@gmail.com>
2016-10-25 13:31:27 -0500

    agregando css al login

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -5,33 +5,15 @@
     .config(($routeProvider,$locationProvider) => {
       $locationProvider.html5Mode(true)
       $routeProvider
+      
       .when('/', {
         templateUrl:'views/principal.html',
         controller:'principalCtrl'
       })
-
-      .when('/quienes', {
-        templateUrl:'views/principal.html',
-        controller:'quienesCtrl'
-      })
-
-      .when('/acerca', {
-        templateUrl:'views/principal.html',
-        controller:'acercaCtrl'
-      })            
-
-      .otherwise({redirectTo:'/' })   
+            
     })
 
     .controller('principalCtrl', function($scope) {
-
-    })
-
-    .controller('queienesCtrl', function(){
-
-    })
-
-    .controller('acercaCtrl', function(){
 
     })
 })()
